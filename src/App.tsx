022acebf47d5363ba6ac69d08454a5b1f2f2f8f0
Денис Путнов <grnbows.connect@mail.ru>
2021-06-10 14:10:49 +0400

    one more try

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,13 @@ import PageNotFound from './components/PageNotFound/PageNotFound'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Layout>
         <Switch>
-          <Route exact path='/markdown-github-blog/' component={MainPage} />
-          <Route exact path='/markdown-github-blog/posts' component={AllPostsPage} />
-          <Route exact path='/markdown-github-blog/404' component={PageNotFound}/>
-          <Route path='/markdown-github-blog/:postname' component={Post}/>
+          <Route exact path='/' component={MainPage} />
+          <Route exact path='/posts' component={AllPostsPage} />
+          <Route exact path='/404' component={PageNotFound}/>
+          <Route path='/:postname' component={Post}/>
         </Switch>
       </Layout>
     </BrowserRouter>
